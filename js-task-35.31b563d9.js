c9e3df9f594f46cad547bd754558ff77dced1b80
elen-purple <anna.havrylova.2011@gmail.com>
2025-04-26 18:05:22 +0300

    Updates

--- js-task-35.31b563d9.js
+++ js-task-35.31b563d9.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"aNPav":[function(require,module,exports,__globalThis) {
+})({"5j6Kf":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 57465;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -667,8 +667,12 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"a0t4e":[function(require,module,exports,__globalThis) {
-function getStudents() {
-    return fetch("http://localhost:3000/students").then((reponse)=>reponse.json());
+async function getStudents() {
+    try {
+        return await fetch("http://localhost:3000/students").then((reponse)=>reponse.json());
+    } catch (e) {
+        return e;
+    }
 }
 function renderStudents(students) {
     document.querySelector("tbody").innerHTML = "";
@@ -692,7 +696,7 @@ function renderStudents(students) {
       </tr>`);
     });
 }
-function addStudent(e) {
+async function addStudent(e) {
     e.preventDefault();
     const student = {
         name: document.querySelector("#name").value,
@@ -707,29 +711,23 @@ function addStudent(e) {
     document.querySelector("#age").value = "";
     document.querySelector("#skills").value = "";
     document.querySelector("#email").value = "";
-    fetch("http://localhost:3000/students", {
-        method: "POST",
-        body: JSON.stringify(student),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        }
-    });
-    setTimeout(()=>{
-        getStudents().then((students)=>{
-            renderStudents(students);
+    try {
+        await fetch("http://localhost:3000/students", {
+            method: "POST",
+            body: JSON.stringify(student),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
         });
-    }, 100);
+    } catch (e) {
+        return e;
+    }
+    await getStudents().then((students)=>{
+        renderStudents(students);
+    });
 }
-// Функція для оновлення студента
-function updateStudent(id) {
-// твій код
-}
-// Функція для видалення студента
-function deleteStudent(id) {
-// твій код
-}
-document.querySelector("#get-students-btn").addEventListener("click", ()=>{
-    getStudents().then((students)=>{
+document.querySelector("#get-students-btn").addEventListener("click", async ()=>{
+    await getStudents().then((students)=>{
         renderStudents(students);
     });
 });
@@ -740,12 +738,12 @@ document.querySelectorAll(`input[type="checkbox"]`).forEach((item)=>{
 });
 document.querySelector("#add-student-form").addEventListener("submit", addStudent);
 let changedUserId = "";
-document.querySelector("tbody").addEventListener("click", (e)=>{
+document.querySelector("tbody").addEventListener("click", async (e)=>{
     if (e.target.classList.contains("change-btn")) {
         document.querySelector("#change").classList.remove("is-hidden");
         document.querySelector("body").classList.add("no-scroll");
         const studentId = e.target.parentElement.parentElement.firstElementChild.textContent;
-        getStudents().then((students)=>{
+        await getStudents().then((students)=>{
             const student = students.find((student)=>student.id === studentId);
             changedUserId = student.id;
             document.querySelector("#change-name").value = student.name;
@@ -757,7 +755,7 @@ document.querySelector("tbody").addEventListener("click", (e)=>{
         });
     }
 });
-document.querySelector("#change-form").addEventListener("submit", (e)=>{
+document.querySelector("#change-form").addEventListener("submit", async (e)=>{
     e.preventDefault();
     const student = {
         id: changedUserId,
@@ -768,14 +766,6 @@ document.querySelector("#change-form").addEventListener("submit", (e)=>{
         email: document.querySelector("#change-email").value,
         isEnrolled: document.querySelector("#change-isEnrolled").hasAttribute("checked")
     };
-    const studentItem = document.querySelector(`#id-${changedUserId}`);
-    studentItem.children[1].textContent = document.querySelector("#change-name").value;
-    studentItem.children[2].textContent = document.querySelector("#change-age").value;
-    studentItem.children[3].textContent = document.querySelector("#change-course").value;
-    const skills = document.querySelector("#change-skills").value.split(",").map((skill)=>`<li>${skill.trim()}</li>`).join("");
-    studentItem.children[4].firstElementChild.innerHTML = skills;
-    studentItem.children[5].textContent = document.querySelector("#change-email").value;
-    studentItem.children[6].textContent = student.isEnrolled ? "\u0417\u0430\u043F\u0438\u0441\u0430\u043D\u0438\u0439" : "\u041D\u0435 \u0437\u0430\u043F\u0438\u0441\u0430\u043D\u0438\u0439";
     document.querySelector("#change").classList.add("is-hidden");
     document.querySelector("body").classList.remove("no-scroll");
     document.querySelector("#change-name").value = "";
@@ -783,47 +773,37 @@ document.querySelector("#change-form").addEventListener("submit", (e)=>{
     document.querySelector("#change-age").value = "";
     document.querySelector("#change-skills").value = "";
     document.querySelector("#change-email").value = "";
-    fetch(`http://localhost:3000/students/${changedUserId}`, {
-        method: "PUT",
-        body: JSON.stringify(student),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        }
+    try {
+        await fetch(`http://localhost:3000/students/${changedUserId}`, {
+            method: "PUT",
+            body: JSON.stringify(student),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        });
+    } catch (e) {
+        return e;
+    }
+    await getStudents().then((students)=>{
+        renderStudents(students);
     });
 });
-document.querySelector("tbody").addEventListener("click", (e)=>{
+document.querySelector("tbody").addEventListener("click", async (e)=>{
     if (e.target.classList.contains("delete-btn")) {
         const studentId = e.target.parentElement.parentElement.firstElementChild.textContent;
-        document.querySelector(`#id-${studentId}`).remove();
-        fetch(`http://localhost:3000/students/${studentId}`, {
-            method: "DELETE"
+        try {
+            await fetch(`http://localhost:3000/students/${studentId}`, {
+                method: "DELETE"
+            });
+        } catch (e) {
+            return e;
+        }
+        await getStudents().then((students)=>{
+            renderStudents(students);
         });
     }
-}); // 1. Реалізуйте функцію getStudents для отримання списку всіх студентів
- // (HTTP GET /students) getStudents
- // 2. Реалізуйте функцію addStudent для додавання нового студента (HTTP
- // POST /students)
- // 3. Реалізуйте функцію updateStudent  для часткового оновлення студента
- // (HTTP PATCH /students/{id})
- // 4. Реалізуйте функцію  для deleteStudent видалення студента за його
- // ідентифікатором (HTTP DELETE /students/{id})
- // 7. Написати JavaScript-код для обробки подій користувача.
- // 7.1. Додати обробники подій для кнопок, щоб вони виконували відповідні
- // HTTP-запити.
- // 7.2. При натисканні на кнопку "Отримати студентів" (GET), виконати
- // HTTP-запит GET /students і відобразити отримані дані в таблиці.
- // 7.3. Реалізувати форму для додавання нового студента. При натисканні
- // на кнопку "Додати студента" (POST), зібрати дані з полів вводу,
- // сформувати об'єкт з даними  і виконати HTTP-запит POST /students, щоб
- // додати нового студента до бази даних.
- // 7.4. Реалізувати можливість оновлення інформації про студента.
- // Для кожного студента в таблиці додати кнопку "Оновити". При натисканні
- // на цю кнопку, виконати HTTP-запит PUT /students/:id, де :id —
- // ідентифікатор фільму, і відправити оновлені дані про студента на сервер.
- // 7.5. Додати можливість видалення студента. Для кожного студента в
- // таблиці додати кнопку "Видалити". При натисканні на цю кнопку, виконати
- // HTTP-запит DELETE /students/:id.
+});
 
-},{}]},["aNPav","a0t4e"], "a0t4e", "parcelRequired62e", {})
+},{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequired62e", {})
 
 //# sourceMappingURL=js-task-35.31b563d9.js.map
